f65ec61e0fdcebc9d572d1b66a2d31291c8240ed
Nelson Tsa <t.nelson@hotmail.fr>
2025-09-01 15:07:18 +0200

    ADD : ajout logique page détail lors du clique sur une card

--- surf_spots_app/lib/widgets/spot_card.dart
+++ surf_spots_app/lib/widgets/spot_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:surf_spots_app/models/surf_spot.dart';
 import 'package:surf_spots_app/constants/colors.dart';
+import 'package:surf_spots_app/pages/spot_detail_page.dart'; // Ajoute l'import
 
 class SpotCard extends StatefulWidget {
   final SurfSpot spot;
@@ -15,40 +16,52 @@ class SpotCard extends StatefulWidget {
 class _SpotCardState extends State<SpotCard> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4.0,
-      clipBehavior: Clip.antiAlias,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(child: Image.asset(widget.spot.imageUrl, fit: BoxFit.cover)),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Text(
-                  widget.spot.name,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Text(widget.spot.description),
-                if (widget.showLike)
-                  IconButton(
-                    icon: Icon(
-                      widget.spot.isLiked ?? false
-                          ? Icons.favorite
-                          : Icons.favorite_border,
-                      color: AppColors.primary,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        widget.spot.isLiked = !(widget.spot.isLiked ?? false);
-                      });
-                    },
-                  ),
-              ],
-            ),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => SpotDetailPage(spot: widget.spot),
           ),
-        ],
+        );
+      },
+      child: Card(
+        elevation: 4.0,
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Image.asset(widget.spot.imageUrl, fit: BoxFit.cover),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(
+                    widget.spot.name,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(widget.spot.description),
+                  if (widget.showLike)
+                    IconButton(
+                      icon: Icon(
+                        widget.spot.isLiked ?? false
+                            ? Icons.favorite
+                            : Icons.favorite_border,
+                        color: AppColors.primary,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          widget.spot.isLiked = !(widget.spot.isLiked ?? false);
+                        });
+                      },
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
