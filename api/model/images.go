c40570290b17c7e78a4ece882fbc24a28fd416ef
ewhalgand <halgandewen44@gmail.com>
2025-09-03 15:21:14 +0200

    fix: images model

--- api/model/images.go
+++ api/model/images.go
@@ -1,9 +1,9 @@
 package model
 
 type Images struct {
-	ID     int    `gorm:"primaryKey" json:"id"`
-	Image  []byte `gorm:"type:bytea;not null" json:"image_data"`
-	SpotID int    `gorm:"not null" json:"spot_id"`
+	ID        int    `gorm:"primaryKey" json:"id"`
+	ImageData []byte `gorm:"type:bytea;not null" json:"image_data"`
+	SpotID    int    `gorm:"not null" json:"spot_id"`
 
 	// Relation
 	Spot *Spots `gorm:"foreignKey:SpotID" json:"spot,omitempty"`
