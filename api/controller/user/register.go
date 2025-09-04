5bd85b38525456248ff282b81199abd0377244fb
leoV0 <leo.cigale@gmail.com>
2025-09-04 14:42:44 +0200

    add: regex to email and password when you create an account

--- api/controller/user/register.go
+++ api/controller/user/register.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"net/http"
+	"regexp"
 	"surf_spots_app/model"
 
 	"github.com/gin-gonic/gin"
@@ -20,10 +21,29 @@ func (h *UserHandler) RegisterUsers(ctx *gin.Context) {
 	}
 
 	if data["email"] == "" || data["password"] == "" || data["name"] == "" {
-        ctx.JSON(http.StatusBadRequest, gin.H{"error": "Missing required fields"})
-        return
-    }
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Missing required fields"})
+		return
+	}
 
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
+	if !emailRegex.MatchString(data["email"]) {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid email format"})
+		return
+	}
+
+	password := data["password"]
+
+	lengthOK := len(password) >= 8
+	upperOK := regexp.MustCompile(`[A-Z]`).MatchString(password)
+	lowerOK := regexp.MustCompile(`[a-z]`).MatchString(password)
+	digitOK := regexp.MustCompile(`\d`).MatchString(password)
+
+	if !(lengthOK && upperOK && lowerOK && digitOK) {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "Password must be at least 8 characters, include upper/lowercase and a number",
+		})
+		return
+	}
 
 	hash, err := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
 	if err != nil {
@@ -34,19 +54,16 @@ func (h *UserHandler) RegisterUsers(ctx *gin.Context) {
 	}
 
 	role := data["role"]
-	if role == ""{
+	if role == "" {
 		role = "user"
 	}
-	
 
 	user := model.Users{
-		Name: data["name"], 
-		Email: data["email"], 
-		Password: hash, 
-		Role: role,
+		Name:     data["name"],
+		Email:    data["email"],
+		Password: hash,
+		Role:     role,
 	}
-
-	
 
 	result := h.DB.Create(&user)
 	if result.Error != nil {
@@ -60,6 +77,6 @@ func (h *UserHandler) RegisterUsers(ctx *gin.Context) {
 		"id":    user.ID,
 		"name":  user.Name,
 		"email": user.Email,
-		"role": user.Role,
+		"role":  user.Role,
 	})
 }
