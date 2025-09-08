0431aff8b9739a1622d17ceb5c64398c9e99a445
ewhalgand <halgandewen44@gmail.com>
2025-09-08 09:53:00 +0200

    feat: add user profile

---
+++ api/controller/user/get-user.go
@@ -0,0 +1,25 @@
+package user
+
+import (
+	"surf_spots_app/model"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h *UserHandler) GetUser(ctx *gin.Context) {
+	userRaw, exists := ctx.Get("user")
+	if !exists {
+		ctx.JSON(401, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	user, ok := userRaw.(model.Users)
+	if !ok {
+		ctx.JSON(500, gin.H{"error": "Internal server error"})
+		return
+	}
+
+	ctx.JSON(200, gin.H{
+		"user": user,
+	})
+}
